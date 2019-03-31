b9d011869e91a711f25be1efa25265794778048a
Pluto Solutions <john@pluto.solutions>
2019-03-31 23:14:02 +0700

    first commit

---
+++ src/main/java/pluto/solutions/App.java
@@ -0,0 +1,13 @@
+package pluto.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
