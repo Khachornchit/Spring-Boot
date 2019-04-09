a41aeda07cac8b456226a8d4d38492bfedbc6b0d
Pluto Solutions <john@pluto.solutions>
2019-04-10 00:30:22 +0700

    Add Tomcat Servlet

--- src/main/java/pluto/solutions/SprintBootQuickStartApp.java
+++ src/main/java/pluto/solutions/SprintBootQuickStartApp.java
@@ -1,13 +1,14 @@
 package pluto.solutions;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SprintBootQuickStartApp {
+public class SprintBootQuickStartApp extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SprintBootQuickStartApp.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(SprintBootQuickStartApp.class, args);
+//	}
 
 }
